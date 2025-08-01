39d61090a665be3bfe7ac1fe0a2b56b4a0eeee4f
RUSH-TD <rushtd1617@gmail.com>
2025-08-01 10:05:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: "", // Put your session id here
+  SESSION_ID: "vJ8AFLaD#gI60XfNE5oge9HNuUnEu8PkgbEQli5rhL5DIyWKsqyU", // Put your session id here
   ALIVE_IMG: "https://github.com/rush1617/SQUID-GAME/blob/main/Images/Alive(2)%20.png?raw=true",
   ALIVE_MSG: `╔═══◉ *🟢 STATUS: ONLINE* ◉═══╗
 ║  𝙷𝚎𝚢 𝙳𝚞𝚍𝚎, 𝙸’𝚖 𝚑𝚎𝚛𝚎 𝚝𝚘 𝚑𝚎𝚕𝚙 𝚢𝚘𝚞.  
